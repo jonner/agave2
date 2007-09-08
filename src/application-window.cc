3e19f0f47bf9f2dab2b0a166043befafaa930c33
Jonathon Jongsma <jjongsma@gnome.org>
2007-09-07 22:19:07 -0500

    Add basic menu bar and AboutDialog Add a basic menu bar skeleton to the application and add actions for 'quit' and 'about'

--- src/application-window.cc
+++ src/application-window.cc
@@ -23,8 +23,15 @@
  *
  *******************************************************************************/
 
+#include <boost/shared_ptr.hpp>
 #include <gtkmm/window.h>
 #include <gtkmm/label.h>
+#include <gtkmm/uimanager.h>
+#include <gtkmm/box.h>
+#include <gtkmm/menu.h>
+#include <gtkmm/stock.h>
+#include <gtkmm/aboutdialog.h>
+#include <glib/gi18n.h>
 #include "config.h"
 #include "application-window.h"
 #include "util.h"
@@ -32,11 +39,53 @@
 namespace agave
 {
 
+    const Glib::ustring menus =
+        "<ui>"
+        "<menubar name='MainMenu'>"
+        "<menu action='MenuAgave'>"
+        "<menuitem action='Quit'/>"
+        "</menu>"
+        "<menu action='MenuHelp'>"
+        "<menuitem action='About'/>"
+        "</menu>"
+        "</menubar>"
+        "</ui>";
+
     struct ApplicationWindow::Priv : public Gtk::Window
     {
-        Priv ()
+        Glib::RefPtr<Gtk::UIManager> m_ui_manager;
+        Glib::RefPtr<Gtk::ActionGroup> m_actions;
+        boost::shared_ptr<Gtk::VBox> m_vbox;
+        Priv () :
+            m_ui_manager (Gtk::UIManager::create ()),
+            m_actions (Gtk::ActionGroup::create ()),
+            m_vbox (new Gtk::VBox)
         {
+            init_actions ();
+            m_ui_manager->add_ui_from_string (menus);
+            m_ui_manager->insert_action_group (m_actions);
             set_title (PACKAGE_NAME);
+            // dynamic_cast doesn't work here for some reason
+            Gtk::Menu* main_menu = static_cast<Gtk::Menu*> (m_ui_manager->get_widget ("/MainMenu"));
+            g_assert (m_vbox);
+            add (*m_vbox);
+            g_assert (main_menu);
+            m_vbox->pack_start (*main_menu, Gtk::PACK_SHRINK);
+            show_all ();
+        }
+
+        void init_actions ()
+        {
+            m_actions->add (
+                    Gtk::Action::create ("MenuAgave", "_Agave"));
+            m_actions->add (
+                    Gtk::Action::create ("Quit", Gtk::Stock::QUIT),
+                    sigc::mem_fun (this, &Priv::on_action_quit));
+            m_actions->add (
+                    Gtk::Action::create ("MenuHelp", _("_Help")));
+            m_actions->add (
+                    Gtk::Action::create ("About", Gtk::Stock::ABOUT),
+                    sigc::mem_fun (this, &Priv::on_action_help_about));
         }
 
         void set_subtitle (const Glib::ustring& subtitle)
@@ -51,6 +100,25 @@ namespace agave
                 title = subtitle + " - " + PACKAGE_NAME;
             }
             set_title (title);
+        }
+
+        void on_action_quit ()
+        {
+            printf ("quitting\n");
+            hide ();
+        }
+
+        void on_action_help_about ()
+        {
+            Gtk::AboutDialog dialog;
+            dialog.set_name (PACKAGE_NAME);
+            dialog.set_version (PACKAGE_VERSION);
+            dialog.set_comments (_("A Colorscheme Designer for the GNOME Desktop"));
+            //dialog.set_logo_icon_name ("agave-icon");
+            std::vector<Glib::ustring> authors;
+            authors.push_back ("Jonathon Jongsma");
+            dialog.set_authors (authors);
+            dialog.run ();
         }
     };
 
