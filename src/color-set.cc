3f2379d86aac1de53d0c093703a88fd0a5075266
Jonathon Jongsma <jjongsma@gnome.org>
2008-03-07 12:29:02 -0600

    Add new ColorSet class for representing saved color schemes

---
+++ src/color-set.cc
@@ -0,0 +1,107 @@
+/*******************************************************************************
+ *
+ *  Copyright (c) 2008 Jonathon Jongsma
+ *
+ *  This file is part of Agave
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses/>
+ *
+ *******************************************************************************/
+
+#include <algorithm>
+#include "color-set.h"
+
+namespace agave
+{
+    ColorSet::ColorSet ()
+    {
+    }
+
+    Glib::ustring ColorSet::get_name () const
+    {
+        return m_name;
+    }
+
+    void ColorSet::set_name (Glib::ustring name)
+    {
+        m_name = name;
+    }
+
+    Glib::ustring ColorSet::get_description () const
+    {
+        return m_description;
+    }
+
+    void ColorSet::set_description (Glib::ustring description)
+    {
+        m_description = description;
+    }
+
+    void ColorSet::add_tag (Glib::ustring tag)
+    {
+        std::list<Glib::ustring>::iterator iter =
+            std::find (m_tags.begin (), m_tags.end (), tag);
+        if (iter != m_tags.end ())
+        {
+            m_tags.push_back (tag);
+        }
+    }
+
+    void ColorSet::remove_tag (Glib::ustring tag)
+    {
+        std::list<Glib::ustring>::iterator iter =
+            std::find (m_tags.begin (), m_tags.end (), tag);
+        if (iter != m_tags.end ())
+        {
+            m_tags.erase (iter);
+        }
+    }
+
+    std::list<Glib::ustring> ColorSet::get_tags () const
+    {
+        return m_tags;
+    }
+
+    void ColorSet::set_colors (const std::list<Color>& colors)
+    {
+        m_colors.assign (colors.begin (), colors.end ());
+    }
+
+    std::list<Color> ColorSet::get_colors () const
+    {
+        return m_colors;
+    }
+
+    void ColorSet::clear ()
+    {
+        m_name.clear ();
+        m_description.clear ();
+        m_tags.clear ();
+        m_colors.clear ();
+    }
+
+    std::ostream& operator<<(std::ostream& out, const ColorSet& s)
+    {
+        out << "Name: " << s.get_name () << std::endl;
+        out << "Description: " << s.get_description () << std::endl;
+        std::list<Color>::const_iterator color_iter;
+        for (color_iter = s.m_colors.begin ();
+                color_iter != s.m_colors.end ();
+                ++color_iter)
+        {
+            out << *color_iter << std::endl;
+        }
+        return out;
+    }
+}
---
+++ src/color-set.h
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ *
+ *  Copyright (c) 2008 Jonathon Jongsma
+ *
+ *  This file is part of Agave
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses/>
+ *
+ *******************************************************************************/
+#ifndef __COLOR_SET_H
+#define __COLOR_SET_H
+
+#include <list>
+#include <glibmm/ustring.h>
+#include "color.h"
+
+namespace agave
+{
+    class ColorSet
+    {
+        public:
+            ColorSet ();
+            Glib::ustring get_name () const;
+            void set_name (Glib::ustring name);
+            Glib::ustring get_description () const;
+            void set_description (Glib::ustring description);
+            void add_tag (Glib::ustring tag);
+            void remove_tag (Glib::ustring tag);
+            std::list<Glib::ustring> get_tags () const;
+            void set_colors (const std::list<Color>& colors);
+            std::list<Color> get_colors () const;
+            void clear ();
+
+            friend std::ostream& operator<<(std::ostream& out, const ColorSet& s);
+
+        protected:
+        private:
+            Glib::ustring m_name;
+            Glib::ustring m_description;
+            std::list<Glib::ustring> m_tags;
+            std::list<Color> m_colors;
+    };
+}
+
+#endif // __COLOR_SET_H
